49a256aee648d8fa9743bcd6955211e6b5c1bc3a
Jeffery Walto <jwalto@msudenver.edu>
2019-06-05 16:16:54 -0600

    Update math

--- BasicMath.py
+++ BasicMath.py
@@ -1,8 +1,6 @@
-num1 = input("Enter one number")
-num2 = input("Enter another number")
+num1 = input("Enter one number: ")
+num2 = input("Enter another number: ")
 
 numTotal = int(num1) + int(num2)
 
-numTotal2 = str(numTotal) + num2
-
-print("Your total is: " + numTotal2)
+print("Your total is: " + str(numTotal))
