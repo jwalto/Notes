20a103f70e29da47039b48bd5f62d45a5de5d6f2
Jeffery Walto <jwalto@msudenver.edu>
2019-06-05 12:18:41 -0600

    test now

---
+++ BasicMath.py
@@ -0,0 +1,8 @@
+num1 = input("Enter one number")
+num2 = input("Enter another number")
+
+numTotal = int(num1) + int(num2)
+
+numTotal2 = str(numTotal) + num2
+
+print("Your total is: " + numTotal2)
