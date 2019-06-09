12afe19d0abaadf917831ac7b1a05453a0ee9e66
Jeffery Walto <jwalto@msudenver.edu>
2019-06-09 15:16:10 -0600

    Test maze

--- Testmain.py
+++ Testmain.py
@@ -4,6 +4,7 @@ import sys
 import random
 import time
 import os
+import maze01
 
 screen_width = 100
 
@@ -53,108 +54,10 @@ def help_menu():
     print('use up, down, left, right to move')
     title_secreen_selections()
 
-ZONENAME = ''
-DESCRIPTION = 'description'
-EXAMINATION = 'examine'
-SOLVED = False
-UP = 'up', 'north'
-DOWN = 'down', 'south'
-LEFT = 'left', 'west'
-RIGHT = 'right', 'east'
-
-solved_places = {'a1': False, 'a2': False, 'a3': False, 'a4': False,
-                'b1': False, 'b2': False, 'b3': False, 'b4': False,
-                'c1': False, 'c2': False, 'c3': False, 'c4': False,
-                'd1': False, 'd2': False, 'd3': False, 'd4': False,
-                }
-
-zonemap = {
-    'a1': {
-        ZONENAME: "a1",
-        DESCRIPTION: 'description',
-        EXAMINATION: 'examine',
-        SOLVED: False,
-        UP: '',
-        DOWN: 'b1',
-        LEFT: '',
-        RIGHT: 'a2',
-    },
-    'a2': {
-        ZONENAME: "a2",
-        DESCRIPTION: 'description',
-        EXAMINATION: 'examine',
-        SOLVED: False,
-        UP: '',
-        DOWN: 'b2',
-        LEFT: 'a1',
-        RIGHT: 'a3',
-    },
-    'a3': {
-        ZONENAME: "a3",
-        DESCRIPTION: 'description',
-        EXAMINATION: 'examine',
-        SOLVED: False,
-        UP: '',
-        DOWN: 'b3',
-        LEFT: 'a2',
-        RIGHT: 'a4',
-    },
-    'a4': {
-        ZONENAME: "a4",
-        DESCRIPTION: 'description',
-        EXAMINATION: 'examine',
-        SOLVED: False,
-        UP: '',
-        DOWN: 'b4',
-        LEFT: 'a3',
-        RIGHT: '',
-    },
-    'b1': {
-        ZONENAME: "b1",
-        DESCRIPTION: 'description',
-        EXAMINATION: 'examine',
-        SOLVED: False,
-        UP: 'a1',
-        DOWN: 'c1',
-        LEFT: '',
-        RIGHT: 'b2',
-    },
-    'b2': {
-        ZONENAME: "b2",
-        DESCRIPTION: 'description',
-        EXAMINATION: 'examine',
-        SOLVED: False,
-        UP: 'b2',
-        DOWN: 'c2',
-        LEFT: 'b1',
-        RIGHT: 'b3',
-    },
-    'b3': {
-        ZONENAME: "b3",
-        DESCRIPTION: 'description',
-        EXAMINATION: 'examine',
-        SOLVED: False,
-        UP: 'a3',
-        DOWN: 'c3',
-        LEFT: 'b2',
-        RIGHT: 'b4',
-    },
-    'b4': {
-        ZONENAME: "b4",
-        DESCRIPTION: 'description',
-        EXAMINATION: 'examine',
-        SOLVED: False,
-        UP: 'a4',
-        DOWN: 'c4',
-        LEFT: 'b3',
-        RIGHT: '',
-    },    
-}
-
 def print_location():
     print('\n' + ('#' * (4 + len(myPlayer.location))))
     print('#' + myPlayer.location.upper() + '#')
-    print('#' + zonemap[myPlayer.location][DESCRIPTION] + '#')
+    print('#' + maze01.zonemap[myPlayer.location][maze01.DESCRIPTION] + '#')
     print('\n' + ('#' * (4 + len(myPlayer.location))))
 
 def prompt():
@@ -176,16 +79,16 @@ def player_move(myAction):
     ask = "Where would you like to move to?\n"
     dest = input(ask)
     if dest in ['up', 'north']:
-        destination = zonemap[myPlayer.location][UP]
+        destination = maze01.zonemap[myPlayer.location][maze01.UP]
         movement_handler(destination)
     elif dest in ['down', 'south']:
-        destination = zonemap[myPlayer.location][DOWN]
+        destination = maze01.zonemap[myPlayer.location][maze01.DOWN]
         movement_handler(destination)
     elif dest in ['left', 'west']:
-        destination = zonemap[myPlayer.location][LEFT]
+        destination = maze01.zonemap[myPlayer.location][maze01.LEFT]
         movement_handler(destination)
     elif dest in ['right', 'east']:
-        destination = zonemap[myPlayer.location][RIGHT]
+        destination = maze01.zonemap[myPlayer.location][maze01.RIGHT]
         movement_handler(destination)
 
 def movement_handler(destination):
@@ -194,7 +97,7 @@ def movement_handler(destination):
     print_location()
 
 def player_examine(action):
-    if zonemap[myPlayer.location][SOLVED]:
+    if maze01.zonemap[myPlayer.location][maze01.SOLVED]:
         print("You have already exausted this zone")
     else:
         print("You can trigger puzzle here")
